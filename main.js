21397569e2e4aed13a9a726ce8d1c890acf69131
Flavie Harre <harreflavie@gmail.com>
2024-03-13 01:02:39 +0100

    Animations des boutons de contact faites

--- main.js
+++ main.js
@@ -21,42 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 const PREV_ARROW = CAT_CONTAINER.querySelector('.prev-arrow');
                 const NEXT_ARROW = CAT_CONTAINER.querySelector('.next-arrow');
                 
-            PREV_ARROW.classList.toggle('active');
-            NEXT_ARROW.classList.toggle('active');
+            // PREV_ARROW.classList.toggle('active');
+            // NEXT_ARROW.classList.toggle('active');
             
             });
 
         });
 
-
-// DOWNLOAD AT CLICK
-
-
-const downloadLink = document.getElementById('CV');
-
-    downloadLink.addEventListener('click', (event) => {
-
-        // Perform download
-        const url = downloadLink.getAttribute('href');
-        const filename = url.substring(url.lastIndexOf('/') + 1);
-        downloadFile(url, filename);
     });
-});
-
-function downloadFile(url, filename) {
-    fetch(url)
-        .then(response => response.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = filename;
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-        })
-        .catch(error => console.error('Error downloading file:', error));
-
-
-    };
 
