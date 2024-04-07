1ac23f6291ba9b29bc6a5122fd8cc1a89a6691c8
Flavie Harre <harreflavie@gmail.com>
2024-04-07 16:31:56 +0200

    index and css updated (not header)

--- main.js
+++ main.js
@@ -1,60 +1,162 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const CAT = document.querySelectorAll('.cat');
 
-
-// Collapse
-    CAT.forEach(CAT => {
-        const CAT_NAME = CAT.querySelector('.cat-name');
-
-
-        CAT_NAME.addEventListener('click', event => {
-            event.stopPropagation(); // Preventing the event from bubbling up to the document
-            CAT_NAME.classList.toggle('active');
-            
-            const CAT_CONTAINER = CAT_NAME.nextElementSibling;
-                if(CAT_NAME.classList.contains('active')) {
-                    CAT_CONTAINER.style.maxHeight = CAT_CONTAINER.scrollHeight + "px";
-                }
-                else {
-                    CAT_CONTAINER.style.maxHeight = 0;
-                }
-                const PREV_ARROW = CAT_CONTAINER.querySelector('.prev-arrow');
-                const NEXT_ARROW = CAT_CONTAINER.querySelector('.next-arrow');
-                
-            // PREV_ARROW.classList.toggle('active');
-            // NEXT_ARROW.classList.toggle('active');
-            
-            });
-
-
-        });
-
+    function isInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+          rect.top <= (window.innerHeight)
+      );
+  }
+  
+  function handleScroll() {
+    let animatedAll = document.querySelectorAll('.to-animate');
+  
+    animatedAll.forEach(animated => {
+      if (isInViewport(animated)) {
+        animated.classList.add('animate');
+      }
     });
-
-
-    // Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal
-let images = document.querySelectorAll(".img");
-let modalImg = document.getElementById("img01");
-
-// Loop through each image and add a click event listener
-images.forEach(img => {
-  img.addEventListener("click", function() {
-    modal.style.display = "block";
-    let srcWithoutMin = this.src.replace("-min", ""); // Remove "-min" from the source
-    modalImg.src = srcWithoutMin;
+  }
+  
+  // Ajouter un gestionnaire de scroll
+  window.addEventListener('scroll', handleScroll);
+  
+  // Gérer les animations au chargement initial de la page
+  handleScroll();
+  
+  
+      // Get the modal
+      let modal = document.getElementById("myModal");
+  
+      // Get the image and insert it inside the modal
+      let images = document.querySelectorAll("#LFP .img");
+      let modalImg = document.getElementById("img01");
+      
+      // Loop through each image and add a click event listener
+      images.forEach(img => {
+        img.addEventListener("click", function() {
+          openModal(this.src);
+        });
+      });
+      
+      let imgButtons = document.querySelectorAll('#creations button');
+      let imagesCreation = document.querySelectorAll("#creations .img");
+      
+      // Loop through each button and add a click event listener
+      imgButtons.forEach(button => {
+        button.addEventListener("click", function() {
+          let imgIndex = Array.from(imgButtons).indexOf(this);
+          let correspondingImg = imagesCreation[imgIndex];
+          let src = correspondingImg.src;
+          if (src.includes("recette_mockup.png")) {
+            src = src.replace("recette_mockup.png", "recette.jpg");
+        }
+          openModal(src);
+        });
+      });
+      
+      // Function to open the modal and set the modal image source
+      function openModal(src) {
+        modal.style.display = "flex";
+        let srcWithoutMin = src.replace("-min", "");
+        modalImg.src = srcWithoutMin;
+      }
+      
+      
+      let closeButton = document.getElementById('close');
+      
+      closeButton.onclick = function() {
+          modal.style.display = "none";
+          modalImg.classList.remove("zoomed");
+          modalImg.style.cursor = "auto";
+        }
+      
+        modal.addEventListener("click", function(event) {
+          if (event.target === modal) {
+            modal.style.display = "none";
+            modalImg.classList.remove("zoomed");
+            modalImg.style.cursor = "auto";
+          }
+      });
+      
+      
+      // Zoom function
+          
+      
+        modalImg.addEventListener("click", function() {
+          if (modalImg.src.includes("quincaillerie") || modalImg.src.includes("recette")) {
+            modalImg.classList.toggle("zoomed");
+            
+        }
+        });
+  
+        modalImg.addEventListener('mouseenter', function() {
+          if (modalImg.classList.contains("zoomed")) {
+            modalImg.style.cursor = "crosshair";
+          } else {
+            if (modalImg.src.includes("quincaillerie") || modalImg.src.includes("recette")) {
+              modalImg.style.cursor = "zoom-in";
+            }
+            else {
+              modalImg.style.cursor = "default";
+  
+            }
+            
+          }
+        });
+        
+        modalImg.addEventListener('mouseleave', function() {
+          if (!modalImg.classList.contains("zoomed")) {
+            modalImg.style.cursor = "default";
+          }
+        });
+        
+        modalImg.addEventListener('click', function() {
+          if (modalImg.classList.contains("zoomed")) {
+            modalImg.style.cursor = "crosshair";
+          } else {
+            if (modalImg.src.includes("quincaillerie") || modalImg.src.includes("recette")) {
+              modalImg.style.cursor = "zoom-in";
+            }
+            else {
+              modalImg.style.cursor = "default";
+  
+            }
+            
+          }
+        });
+  
+      
+        modalImg.onmousemove = function(e) {
+        e.target.style.setProperty('--x',(100*e.offsetX/e.target.offsetWidth)+'%');
+        e.target.style.setProperty('--y',(100*e.offsetY/e.target.offsetHeight)+'%'); 
+      }
+  
+  
   });
-});
-
-let closeButton = document.getElementById('close');
-// When the user clicks on <span> (x), close the modal
-
-modal.onclick = function() {
-    modal.style.display = "none";
-  }
-
-closeButton.onclick = function() {
-    modal.style.display = "none";
-  }
+  
+  
+      const CAT = document.querySelectorAll('.cat');
+  
+  
+  // Collapse
+      CAT.forEach(CAT => {
+          const CAT_NAME = CAT.querySelector('.cat-name');
+  
+  
+          CAT_NAME.addEventListener('click', event => {
+              event.stopPropagation(); // Preventing the event from bubbling up to the document
+              CAT_NAME.classList.toggle('active');
+  
+              
+              const CAT_CONTAINER = CAT_NAME.nextElementSibling;
+                  if(CAT_NAME.classList.contains('active')) {
+                      CAT_CONTAINER.style.maxHeight = CAT_CONTAINER.scrollHeight + "px";
+  
+                  }
+                  else {
+                      CAT_CONTAINER.style.maxHeight = 0;
+                  }
+              
+              });
+  
+          });  
